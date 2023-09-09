4d69f35ec3a1898e104f2befb7f413119ecb5bd3
forz <73713286+forzzzzz@users.noreply.github.com>
2023-09-09 20:17:36 +0200

    XD

---
+++ Video Cutter in 5 seconds/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Video_Cutter_in_5_seconds
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 126);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(83, 168);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(18, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Начало\r\nmm:ss";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(18, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Конец\r\nmm:ss";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 269);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 36);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Експорт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(203, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Состояние:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(83, 62);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(243, 20);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(31, 66);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Путь";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(15, 209);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 32);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Битрейт\r\nk/s";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(83, 209);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(206, 118);
+            this.label4.Multiline = true;
+            this.label4.Name = "label4";
+            this.label4.ReadOnly = true;
+            this.label4.Size = new System.Drawing.Size(152, 126);
+            this.label4.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(203, 259);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 16);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "CPU:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(18, 355);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(83, 32);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Путь\r\nсохранения";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(107, 363);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(251, 20);
+            this.textBox5.TabIndex = 13;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(203, 289);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(99, 16);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Температура:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(203, 314);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(73, 16);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Нагрузка:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(299, 289);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(14, 16);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "0";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.Location = new System.Drawing.Point(273, 314);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(14, 16);
+            this.label12.TabIndex = 18;
+            this.label12.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 410);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(372, 318);
+            this.Name = "Form1";
+            this.Text = "Video Cutter in 5 sec";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ Video Cutter in 5 seconds/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using OpenHardwareMonitor.Hardware;
+using System.Threading;
+
+namespace Video_Cutter_in_5_seconds
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormClosing += Form1_FormClosing1;
+        }
+
+        private void Form1_FormClosing1(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.Bit = textBox4.Text;
+            Properties.Settings.Default.save = textBox5.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox4.Text = Properties.Settings.Default.Bit;
+            textBox5.Text = Properties.Settings.Default.save;
+
+            Thread monitoringThread = new Thread(MonitorHardware);
+            monitoringThread.Start();
+        }
+
+
+        void MonitorHardware()
+        {
+            Computer computer = new Computer();
+            computer.Open();
+            computer.CPUEnabled = true;
+
+            while (true)
+            {
+                foreach (var hardware in computer.Hardware)
+                {
+                    if (hardware.HardwareType == HardwareType.CPU)
+                    {
+                        hardware.Update();
+
+                        foreach (var sensor in hardware.Sensors)
+                        {
+                            if (sensor.SensorType == SensorType.Temperature)
+                            {
+                                label11.Invoke(new Action(() => label11.Text = $"{sensor.Value}°C"));
+                            }
+                            if (sensor.SensorType == SensorType.Load)
+                            {
+                                label12.Invoke(new Action(() => label12.Text = $"{sensor.Value}%"));
+                            }
+                        }
+                    }
+                }
+
+                Thread.Sleep(500);
+            }
+        }
+
+
+        //public void Monitor()
+        //{
+        //    Computer computer = new Computer();
+        //    computer.Open();
+
+        //    foreach (var hardware in computer.Hardware)
+        //    {
+        //        Console.WriteLine(hardware.Name);
+        //        if (hardware.HardwareType == HardwareType.CPU)
+        //        {
+        //            hardware.Update();
+        //            foreach (var sensor in hardware.Sensors)
+        //            {
+        //                if (sensor.SensorType == SensorType.Temperature)
+        //                {
+        //                    Console.WriteLine($"CPU Temperature: {sensor.Value}°C");
+        //                }
+        //            }
+        //        }
+        //    }
+
+        //    computer.Close();
+        //}
+
+        static string ConvertTime(string timeStr)
+        {
+            // Разбиваем строку времени на минуты и секунды
+            string[] timeParts = timeStr.Split(':');
+            int minutes = int.Parse(timeParts[0]);
+            int seconds = int.Parse(timeParts[1]);
+
+            // Вычисляем часы и оставшиеся минуты
+            int hours = minutes / 60;
+            minutes = minutes % 60;
+
+            // Возвращаем строку в формате hh:mm:ss
+            return $"{hours:D2}:{minutes:D2}:{seconds:D2}";
+        }
+
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox3.Text == "")
+            {
+                label4.Text = "Пусто";
+            }
+            else
+            {
+                label4.Text = "Занято";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            //Monitor();
+
+
+
+            string input_video = textBox3.Text;
+
+            string fileNameWithExtension = Path.GetFileName(input_video);
+
+            string output_video = textBox5.Text;
+            if (!output_video.EndsWith("\\"))
+            {
+                output_video = output_video + "\\" + fileNameWithExtension;
+            }
+            else
+            {
+                output_video = output_video + fileNameWithExtension;
+            }
+
+            string start_cut = ConvertTime(textBox1.Text);
+            string end_cut = ConvertTime(textBox2.Text);
+
+
+            Process proc = new Process();
+            proc.StartInfo.FileName = "ffmpeg-2023-04-26-git-e3143703e9-full_build\\bin\\ffmpeg.exe";
+            proc.StartInfo.Arguments = $"-i \"{input_video}\" -ss {start_cut} -to {end_cut} -r 60 -b:v {textBox4.Text}k -c:v libx264 -c:a copy \"{output_video}\"";
+            proc.StartInfo.RedirectStandardError = true;
+            proc.StartInfo.CreateNoWindow = true;
+            proc.StartInfo.UseShellExecute = false;
+            if (!proc.Start())
+            {
+                return;
+            }
+
+            async void StartAsyncProcessing()
+            {
+                StreamReader reader = proc.StandardError;
+                string line;
+                while ((line = await reader.ReadLineAsync()) != null)
+                {
+                    if (line == ": Invalid argument")
+                    {
+                        MessageBox.Show("Алооо бебрік недорастер", "Ошибка: нужны мозги!!!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else if(line == "Press [q] to stop, [?] for help")
+                    {
+                        label4.Text = "Подготовка до ддоса олега";
+                    }
+                    else
+                    {
+                        string[] parts = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                        StringBuilder formattedOutput = new StringBuilder();
+
+                        for (int i = 0; i < parts.Length; i++)
+                        {
+                            if (i % 2 == 0 && i != 0)
+                            {
+                                formattedOutput.Append(Environment.NewLine); // Перевод строки между элементами (кроме первого)
+                            }
+                            formattedOutput.Append(parts[i] + " "); // Вывод элемента с пробелом
+                        }
+
+                        label4.Text = formattedOutput.ToString();
+                    }
+                }
+
+                label4.Clear();
+                textBox3.Clear();
+                textBox1.Clear();
+                textBox2.Clear();
+            }
+
+            StartAsyncProcessing();
+
+            proc.Close();
+ 
+        }
+    }
+}
